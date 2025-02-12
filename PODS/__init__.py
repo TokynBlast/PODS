161e3348a28c2908818d7d2c0fac824b62a04807
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-12 18:12:05 +0000

    Added __init__

---
+++ PODS/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['extract']
+
+__url__ = 'https://github.com/TokynBlast/pyTGM'
+__download_url__ = 'https://pypi.org/tokynblast'
+__bug_tracker_url__ = 'https://github.com/TokynBlast/PODS/issues'
+__changelog_url__ = 'https://github.com/TokynBlast/PODS/blob/main/CHANGELOG.txt'
+
+from extract import exctract
